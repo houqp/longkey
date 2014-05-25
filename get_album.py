f478274aaef5691578adbc63e42093a4d1c278ff
Qingping Hou <dave2008713@gmail.com>
2014-05-24 21:51:59 -0400

    fix get_album.py

--- get_album.py
+++ get_album.py
@@ -2,21 +2,35 @@
 # -*- coding:utf-8 -*-
 
 import requests
-import sys
 import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("artist_id", help="artist id", type = str)
+    parser.add_argument("artist_id", help="artist id", type=str)
+    parser.add_argument("-d", help="debug mode", action='store_true')
     args = parser.parse_args()
 
-    r = requests.get('http://www.xiami.com/artist/album/id/'+args.artist_id)
-    page_div = r.content.split('<div class="all_page">')[1].split('</div>')[0]
-    page_num = len(page_div.split('p_num')) - 1
-    #print("Total number of pages: "+str(page_num))
+    headers = {
+        'User-Agent': 'WTF-1.0',
+    }
+    url = 'http://www.xiami.com/artist/album/id/'+args.artist_id
+    r = requests.get(url, headers=headers)
+    try:
+        page_div = r.content.split('<div class="all_page">')[1]\
+                    .split('</div>')[0]
+        page_num = len(page_div.split('p_num')) - 1
+    except IndexError:
+        page_num = 1
+    else:
+        pass
+    if args.d:
+        print("Total number of pages: "+str(page_num))
     for i in range(page_num):
-        #print("parsing page: "+str(i+1))
-        r = requests.get('http://www.xiami.com/artist/album/id/216/d//p//page/'+str(i+1))
+        if args.d:
+            print "parsing page {0}".format(i+1)
+        url = 'http://www.xiami.com/artist/album/id/{0}/d//p//page/{1}'.format(
+            args.artist_id, str(i+1))
+        r = requests.get(url, headers=headers)
         album_list = r.content.split("playalbum('")
         # remove useless header
         album_list.pop(0)
@@ -24,5 +38,3 @@ if __name__ == "__main__":
         for j in range(len(album_list)/2):
             aid = album_list[j][:album_list[j].find("'")]
             print aid
-
-        
